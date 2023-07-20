a59f7ffe355b7f512b47b97b57ec736354b89ff5
Yarra <yarrasree@users.noreply.github.com>
2023-07-21 01:29:38 +0530

    Add files via upload

---
+++ unit_test/constants.py
@@ -0,0 +1,9 @@
+# You can create more variables according to your project. The following are the basic variables that have been provided to you
+DB_PATH = ''
+DB_FILE_NAME = 'utils_output.db'
+UNIT_TEST_DB_FILE_NAME = 'unit_test_cases.db'
+DATA_DIRECTORY = ''
+INTERACTION_MAPPING = 'interaction_mapping.csv'
+#INDEX_COLUMNS = 
+#NOT_FEATURES = 
+
